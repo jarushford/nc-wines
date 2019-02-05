dbf0a2b798b0c2cf957687ac7895af2180a43950
Jamie Rushford <jar.rushford@gmail.com>
2019-02-05 14:53:39 -0700

    Initialize react app, set up header UI

---
+++ src/App/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Header from '../Header/Header'
+
+export default class App extends Component {
+  constructor() {
+    super()
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    )
+  }
+}
+
