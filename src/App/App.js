ce3727d0b6a1667b38ca2a83cc5e68d046256266
Jamie Rushford <jar.rushford@gmail.com>
2019-02-05 20:01:55 -0700

    Clean up spacing

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Header from '../Header/Header'
 import TestForm from '../TestForm/TestForm'
 import APIList from '../APIList/APIList'
 
-export default class App extends Component {
-
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <TestForm />
-        <APIList />
-      </div>
-    )
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <TestForm />
+      <APIList />
+    </div>
+  )
 }
-
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,12 +6,12 @@ export default function Header() {
     <header className="header">
       <h1>North Carolina Wines</h1>
       <button
-        onClick={() => window.scrollTo({ top: 500, behavior: 'smooth' })}
+        onClick={() => window.scrollTo({ top: 720, behavior: 'smooth' })}
       >
         Test Endpoints
       </button>
       <button
-        onClick={() => window.scrollTo({ top: 1900, behavior: 'smooth' })}
+        onClick={() => window.scrollTo({ top: 2100, behavior: 'smooth' })}
       >
         Available Endpoints
       </button>
